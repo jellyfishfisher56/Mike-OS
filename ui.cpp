81bd04762a12f6e10d257f4acded8c7a4eac6533
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-21 08:09:36 +0100

    Create ui.cpp

---
+++ ui.cpp
@@ -0,0 +1,23 @@
+compile in ATEM0_75:
+  walpaoper sdgh65sawqh.jpg
+  square StartMenu
+  display Apps
+  ds.
+  Start.bmp
+  StartMenu.png
+  .apps on taskbar, with titles
+  .apps on start, with titles below
+  searchIndex rq ad fg
+  ap s:
+    bq w
+    as f:
+      sd
+      ms [[]]
+      on jk
+     sa cf
+     on searchIn
+   as
+   on SearchText onlyUI info IO
+   io.jpg
+   st rs
+   op 09
