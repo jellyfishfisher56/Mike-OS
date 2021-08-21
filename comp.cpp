8791ccd778c689ccfc38181b5505bd9a7d401dfe
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-21 07:54:00 +0100

    Create comp.cpp

---
+++ comp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+int main()
+{
+  int myoutput
+  myoutput = com.outnumer
+  std::cout << myoutput // This prints the var myoutput.
+  std::cout << myoutput
+  std::cout << myoutput
+   return 0;
+}
