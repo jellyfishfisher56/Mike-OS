628a5a4da08932059a056d117cd6039e6dd6cc30
jellyfishfisher56 <87594726+jellyfishfisher56@users.noreply.github.com>
2021-08-21 07:59:43 +0100

    Create install.cpp

---
+++ install.cpp
@@ -0,0 +1,13 @@
+compile:
+  text()
+  as(f)
+  inytrofrt(x - 6 ad hj on int plat)
+    on;
+  std
+  background(0x.BLUE)
+    .text
+    .buttons.list
+  onclick install EXCEPT firstStart;
+  EXCEPT::
+  compi
+  ext on ext wipe background;
